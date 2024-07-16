82059c820c425984a56379ad444e664678b8aba6
RooK| <anton.smishko2014@gmail.com>
2024-07-16 10:41:22 +0300

    new script

--- new.js
+++ new.js
@@ -1,1 +1,26 @@
-console.log(typeof typeof 1);
+//console.log(typeof typeof 1);
+
+
+function getFileName(file) {
+
+    let withDot = file.indexOf(".")
+    
+    console.log(withDot)
+
+    if (withDot != -1) {
+
+       return file.slice(withDot);
+        
+    }   else {
+        console.log(file)
+        return file
+    }
+  }
+
+
+ getFileName("s.js")
+
+ let withDot = file.indexOf(".")
+ console.log(file.slice( 0, withDot));
+
+ 
