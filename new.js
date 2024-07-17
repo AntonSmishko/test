9d1d85804364be5b45eb51369d1d87039527e5d8
RooK| <anton.smishko2014@gmail.com>
2024-07-17 19:57:00 +0300

    new code
    function and []

--- new.js
+++ new.js
@@ -1,26 +1,69 @@
-//console.log(typeof typeof 1);
+// const newArray = [ "Mango","Jupiter","apple"];
+
+// console.log(newArray.length -1);
+// console.log(newArray[newArray.length -1]);
 
 
-function getFileName(file) {
+// console.log(Number([1,2])); 
 
-    let withDot = file.indexOf(".")
-    
-    console.log(withDot)
+// function getLength(array) {
+//     const arrayToString = array.join("");
+//     console.log(" this array>", arrayToString)
+//       return arrayToString.length
+//     }
 
-    if (withDot != -1) {
+//    console.log( getLength(["Mango", "hurries", "to", "the", "train"]))
 
-       return file.slice(withDot);
+//    const newArrayTest = ["Mango", "hurries", "to", "the", "train"]
+//    console.log(newArrayTest)
+
+
+
+//    const slug = "amazing french recipes";
+// const slugParts = slug.split(" ");
+// console.log(slugParts); 
+
+
+// Знайти чи є такий ел в масиві а також повернути зі зрізом включаючи цей ел
+
+// function getSlice(array, value) {
+  
+//     if (array.indexOf(value) != -1) {
+//      let indexArrV =  array.indexOf(value);
+//      indexArrV += 1 ;
+      
+//       return   array.slice(0,  indexArrV);
+      
+//     } else {
+//       return []
+//     }
+//   }
+
+//   console.log(getSlice(["Mango", "Poly", "Ajax"], "Ajax"));
+
+
+
+function createArrayOfNumbers(min, max) {
+    let newArray = [];
+    for (let i = min; i <= max; i += 1) {
         
-    }   else {
-        console.log(file)
-        return file
+      console.log(i);
+      newArray.push(i)
+
     }
+
+    return newArray
   }
 
 
- getFileName("s.js")
+  console.log(createArrayOfNumbers(1, 5))
 
- let withDot = file.indexOf(".")
- console.log(file.slice( 0, withDot));
 
- 
+  
+//   const tags = [];
+
+// for(let i = 0; i < 3; i += 1) {
+// 	tags.push(`tag-${i}`);
+// }
+
+// console.log(tags); // ["tag-0", "tag-1", "tag-2"]
